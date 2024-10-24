6d1d9459e3ccc08ef8a529f274a0e6fdea3d3135
saurav-kothale <sauravkothale@gmail.com>
2024-10-24 13:19:39 +0530

    Feature : created model and preprocessing model

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -11,13 +11,14 @@ from src.components.model_trainer import ModelTrainer
 
 
 if __name__=='__main__':
+    
     obj=DataIngestion()
     train_data_path,test_data_path=obj.initiate_data_ingestion()
     print(train_data_path,test_data_path)
 
     data_transformation=DataTransformation()
 
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+    train_arr,test_arr,object_path=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
 
     model_trainer=ModelTrainer()
     model_trainer.initate_model_training(train_arr,test_arr)
